d0e0c633517aa12c86578b6b49b86fd56a86ae82
mrkeeney <mallory-tollefson@uiowa.edu>
2016-03-16 14:33:16 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " The University of Iowa. All rights reserved.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='algorithms.html'>Algorithms</a></li>" +
+"<li><a href='data.html'>Data</a></li>" +
+"</ul>";
